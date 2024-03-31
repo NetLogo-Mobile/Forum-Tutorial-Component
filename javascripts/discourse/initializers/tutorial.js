54068bffd69262c000bc77e9a8aa0b92782d658c
John Chen <john@soobb.com>
2024-03-30 22:17:46 -0500

    Trying to get the tutorial config and yml work

--- javascripts/discourse/initializers/tutorial.js
+++ javascripts/discourse/initializers/tutorial.js
@@ -3,7 +3,13 @@ import { apiInitializer } from "discourse/lib/api";
 
 // Load the tutorial driver script
 async function loadTutorial() {
+	// Load the driver
 	await loadScript(settings.theme_uploads_local.driver_js);
+	const driver = window.driver.js.driver;
+	// Load the config
+	await loadScript(settings.theme_uploads_local.physics_lab);
+	const config = window.discourseTutorial;
+	console.log(config);
 }
 
 // Register the initializer
