7ecc1ba1cc2545a3bac70d99db0447f9a3241a77
xiegushi2022@outlook.com <xiegushi2022@outlook.com>
2024-05-25 13:24:20 +0800

    debug

--- assets/physics-lab-forum.js
+++ assets/physics-lab-forum.js
@@ -288,7 +288,10 @@ window.discourseTutorial = {
         "popover": {
           "title": "关于“管理人员”",
           "description": "有任何意见、建议、反馈都可以随时联系我们的管理人员，如果您希望创建新的标签、类别、群组，也可以发布帖子或者和管理人员一同讨论",
-          "onNextClick": () => document.querySelector(".username").click()
+          "onNextClick": () => {
+            document.querySelector(".username").click(),
+            myDriver.moveNext()
+          }
         }
       },
       {
